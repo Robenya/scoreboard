6bdddbaf5a65e14aecfe7ebdfaa373dbb37eaedd
Robenya <56411385+Robenya@users.noreply.github.com>
2023-06-05 11:36:49 -0400

    reduce code

--- index.js
+++ index.js
@@ -5,13 +5,9 @@ let fightButton = document.getElementById("fightButton")
 let ganador = document.getElementById("ganador-el")
 
 fightButton.addEventListener("click", function() {
-    
-    function getHand(){
-        return hands[Math.floor(Math.random() * hands.length)]
-    }
 
-    let play1 = getHand()
-    let play2 = getHand()
+    let play1 = hands[Math.floor(Math.random() * hands.length)]
+    let play2 = hands[Math.floor(Math.random() * hands.length)]
     stageEl.textContent = play1 + " VS " + play2
 
     if(play1 === "🖐" && play2 === "✌" || play2 === "🖐" && play1 === "✌"){
