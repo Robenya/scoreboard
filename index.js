3911e08ad6874dcabaec0da1ae9f682bc3780d81
Robenya <56411385+Robenya@users.noreply.github.com>
2023-06-05 11:16:02 -0400

    agregar ganadores

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ let hands = ["🖐", "✌","✊"]
 
 let stageEl = document.getElementById("stage")
 let fightButton = document.getElementById("fightButton")
+let ganador = document.getElementById("ganador")
 
 fightButton.addEventListener("click", function() {
+    
     function getHand(){
         return hands[Math.floor(Math.random() * hands.length)]
     }
-    stageEl.textContent = getHand() + " VS " + getHand()
-})
+
+    let play1 = getHand()
+    let play2 = getHand()
+    stageEl.textContent = play1 + " VS " + play2
+
+    if(play1 === "🖐" && play2 === "✌" || play2 === "🖐" && play1 === "✌"){
+        ganador.textContent = "Gana ✌"
+    } else if (play1 === "✊" && play2 === "✌" || play2 === "✊" && play1 === "✌"){
+        ganador.textContent = "Gana ✊"
+    }else if (play1 === "🖐" && play2 === "✊" || play2 === "🖐" && play1 === "✊"){
+        ganador.textContent = "Gana 🖐"
+    } else {
+        ganador.textContent = "Es empate!!"
+    }
+}
+)
