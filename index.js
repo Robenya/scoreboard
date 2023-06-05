fbc5ee3b05b5507d8c892d08ea3199a50ae86109
Robenya <56411385+Robenya@users.noreply.github.com>
2023-06-05 01:25:45 -0400

    arreglo boton again

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 let contador = document.getElementById("contador")
 let cuenta = 0
+contador.textContent = "Usa esto para contar: " + cuenta
 
 function agregar() {
     cuenta += 1
+    contador.textContent = "Usa esto para contar: " + cuenta
 }
 
 function quitar() {
     cuenta -= 1
+    contador.textContent = "Usa esto para contar: " + cuenta
 }
-
-contador.textContent = "Usa esto para contar: " + cuenta
