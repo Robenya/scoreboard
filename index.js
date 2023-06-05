d34526fa3e87d9d652b4e3e9baca5215d5a293ec
Robenya <56411385+Robenya@users.noreply.github.com>
2023-06-05 01:18:17 -0400

    arreglar botones

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 let contador = document.getElementById("contador")
 let cuenta = 0
 
+contador.textContent = cuenta
+
 function agregar() {
     cuenta++
+    contador.textContent = cuenta
 }
 
 function quitar() {
     cuenta--
+    contador.textContent = cuenta
 }
 
-contador.textContent = cuenta
